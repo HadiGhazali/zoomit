0bbfaa320a474eee2f2ec41b08512132e122f16c
hadi <hadivardanjani1378@gmail.com>
2020-12-15 02:22:05 +0330

    update project

--- account/admin.py
+++ account/admin.py
@@ -11,19 +11,25 @@ from account.models import User
 class UserAdmin(BaseUserAdmin):
     change_password_form = AdminPasswordChangeForm
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('profile_name', 'first_name', 'last_name', 'email')}),
+        (None, {'fields': ('phone_number', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        # (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2'),
+            'fields': ('phone_number', 'password1', 'password2'),
         }),
     )
+
+    list_display = ('phone_number', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('phone_number', 'first_name', 'last_name', 'email')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
 
 admin.site.register(User, UserAdmin)
